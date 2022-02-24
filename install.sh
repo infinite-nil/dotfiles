cdd970f32078ba255b9ae709b0e7727e8f64ce29
João Alberto <hey@joaoalberto.dev>
2022-02-24 15:04:15 -0300

    chore: fix symlinks and add plugvim

--- install.sh
+++ install.sh
@@ -5,10 +5,12 @@ if [ "$1" == "--prepare" ]; then
 
 	sudo apt install git vim curl zsh -qq
 	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 else
 	echo "Setup"
 
-	ln -s ~/dotfiles/terminal/.bashrc ~/.bashrc
-	ln -s ~/dotfiles/terminal/.zshrc ~/.zshrc
+	ln -sf ~/dotfiles/terminal/.bashrc ~/.bashrc
+	ln -sf ~/dotfiles/terminal/.zshrc ~/.zshrc
+	ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
 fi
 
